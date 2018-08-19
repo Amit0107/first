5e59220bada2082a4639bde9578c7a6167592f1e
Amit0107 <42513485+Amit0107@users.noreply.github.com>
2018-08-19 17:42:24 +0530

    Add files via upload

---
+++ first/src/js/main.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM form "react-dom";
+class Layout extends React.Component{
+  render() {
+    const name = "will";
+    return (
+      <h1>It's '{{function(){ return})()}!
+  }
+}
